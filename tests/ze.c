457255196fbffc431453bc88ce8394cbf1790e30
Djebril <djebril.harhad@epitech.eu>
2021-03-04 11:39:30 +0100

    l

--- tests/ze.c
+++ tests/ze.c
@@ -6,10 +6,9 @@
 */
 
 #include <criterion/criterion.h>
-
 #include "tek.h"
 
 Test(FUNCTIONAL_TEST, get_first_sentence)
 {
-    cr_assert_str_not_empty(get_sentence(0));
+    cr_assert_str_not_empty(get_sentence(1));
 }
