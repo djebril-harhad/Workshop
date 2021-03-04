c2d6a2a15a1ea9d1b2f53e81f17fea1a8ef01f41
Djebril <djebril.harhad@epitech.eu>
2021-03-04 12:01:23 +0100

    p

---
+++ tests/tests.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** Workshop
+** File description:
+** tests
+*/
+
+#include <criterion/criterion.h>
+#include "tek.h"
+
+Test(stest, get_first_sentence)
+{
+    cr_assert_str_not_empty(get_sentence(1));
+}
